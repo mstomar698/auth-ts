537d4f037991c70b44768293d0112d971bf289be
mstomar698 <tomarm698@gmail.com>
2023-04-18 23:16:38 +0530

    [4] Set DB

--- server.ts
+++ server.ts
@@ -1,15 +1,31 @@
-import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import homeRouter from './routes/home';
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI!)
+  .then(() => {
+    console.log('connected to DB');
+  })
+  .catch((err: Error) => {
+    console.log(err.message);
+  });
+
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', homeRouter);
+
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`serving at http://localhost:${port}`);
 });
